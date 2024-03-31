78f013baa8eb2f51afb8b7ff39bba6a907fef8a3
Yuosif <yuosifraed@gmail.com>
2024-04-01 02:45:24 +0300

    if and else

--- main.dart
+++ main.dart
@@ -1,28 +1,12 @@
 void main() {
-  // varibale
-  var name1 = false;
-//sets
-  var name = <int>{1, 2};
-  name.addAll({12, 4, 42});
-  print(name);
-//const
-  final city = const {'baghdad', 'albasraa'};
-
-  print(city);
-
-//maps
-  var persone = {
-    //key        value
-    2: 'footbal',
-    'yoyo': 'swaming'
-  };
-
-  print(persone['yoyo']);
-
-  var someOne = Map<String, int>();
-  someOne['yoyo'] = 27;
-  someOne['yoyo'] = 28;
-  someOne['bedaa'] = 26;
-
-  print(someOne['yoyo']);
+  // condation
+  var names = ['yoyo', 'rkpor'];
+  // Operators  ==    <=   >=
+  if (names.length <= 4 || !(names[0] == 'yoyo' && names[1] == 'rkpor')) {
+    print(true);
+  } else if (1 > 2) {
+    print('equal');
+  } else {
+    print(false);
+  }
 }
