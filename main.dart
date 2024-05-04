76b6a6619f9ad20e904badfc15694a024a01230e
Yuosif <yuosifraed@gmail.com>
2024-05-05 01:00:38 +0300

    optional params and basic functions

--- main.dart
+++ main.dart
@@ -1,55 +1,12 @@
-import 'dart:ffi';
-
 void main() {
-  int a = 10;
-  int b = 4;
+  print((6 ~/ 2));
 
-  print(a == b ? "is equals" : "Not equal");
+  int a = 1000, b = 2000, h = 3000, t = 3;
 
-  print(a is int ? "Number" : "Not Number");
+  num result = sum(a);
+  print('this result value :' + result.toString());
+}
 
-  bool isAdmin = true;
-  var user = isAdmin ? "this user Admin" : "not admin";
-  print(user);
-
-  String? someOne;
-  someOne = "ahmed";
-  someOne = null;
-
-  int h = 4;
-  double k = 7;
-
-  num sum = h + k;
-
-  // Nested If
-  if (a == 3)
-    print('');
-  else if (isAdmin) print('test');
-
-  // Switch Case
-  String fruit = 'apple';
-
-  switch (fruit) {
-    case 'apple':
-      print('Selected fruit is apple');
-      break;
-    case 'banana':
-      print('Selected fruit is banana');
-      break;
-    case 'orange':
-      print('Selected fruit is orange');
-      break;
-    default:
-      print('Unknown fruit');
-  }
-// uniqe
-  Set<String> names = {'yoyo', 'ahmed'};
-  names.add('mohss');
-  print(names);
-
-  print(names.length);
-  //for loop
-  for (String name in names) {
-    print(name == 'yoyo' ? "founded" : "not found");
-  }
+num sum(int price, [int discount = 10]) {
+  return price - price * discount ~/ 100;
 }
