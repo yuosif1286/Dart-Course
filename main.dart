76f9f5f8fda29423dd45d92c8cd357f20ee08a2a
Yuosif <yuosifraed@gmail.com>
2024-05-05 17:49:07 +0300

    class

--- main.dart
+++ main.dart
@@ -1,12 +1,41 @@
+import 'dart:async';
+
 void main() {
   print((6 ~/ 2));
 
   int a = 1000, b = 2000, h = 3000, t = 3;
 
-  print(sum(n1: 5, discount: 4));
+  car BMW = car();
+  BMW.color = 'black';
+  BMW.name = '2023';
+  BMW.number = 543;
+
+  print(BMW.name! + ' ' + BMW.number.toString() + ' ' + BMW.color!);
+
+  car sonata = car();
+  sonata.color = 'black';
+  sonata.name = '2022';
+  sonata.number = 543;
+  print(sonata.name! + ' ' + sonata.number.toString() + ' ' + sonata.color!);
+
+  Human ahmed = Human();
+  ahmed.haierColor = 'black';
+  ahmed.height = 32;
+  ahmed.myCar = car();
+  ahmed.myCar?.name = 'saibaa';
 }
 
-// name parametter
-num sum({required num n1, num discount = 0}) {
-  return n1 - discount;
+// objeect oriented programing
+//class
+class car {
+  String? name;
+  String? color;
+  num? number;
+}
+
+class Human {
+  String? name;
+  int? height;
+  String? haierColor;
+  car? myCar;
 }
