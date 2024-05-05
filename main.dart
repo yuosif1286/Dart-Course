36cb583be63c5552dc33b709b1e5b5f3b6da9a7c
Yuosif <yuosifraed@gmail.com>
2024-05-05 17:35:05 +0300

    name  paramter

--- main.dart
+++ main.dart
@@ -3,16 +3,10 @@ void main() {
 
   int a = 1000, b = 2000, h = 3000, t = 3;
 
-  num result = sum(a);
-  print('this result value :' + result.toString());
-
-  print(min(number1: 4, number2: 3));
+  print(sum(n1: 5, discount: 4));
 }
 
-num sum(int price, [int discount = 10, String? name]) {
-  return price - price * discount ~/ 100;
-}
-
-num min({required num number1, num number2 = 0}) {
-  return number1 - number2;
+// name parametter
+num sum({required num n1, num discount = 0}) {
+  return n1 - discount;
 }
