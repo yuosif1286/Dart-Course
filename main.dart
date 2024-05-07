bc242b7320742be707a8ff65bd06c590d7553b01
Yuosif <yuosifraed@gmail.com>
2024-05-08 01:15:33 +0300

    method

--- main.dart
+++ main.dart
@@ -1,41 +1,30 @@
 import 'dart:async';
 
 void main() {
-  print((6 ~/ 2));
+  Human bedaa = Human();
 
-  int a = 1000, b = 2000, h = 3000, t = 3;
+  bedaa.name = 'bedaa ali';
+  bedaa.haierColor = 'black';
+  bedaa.height = 150;
 
-  car BMW = car();
-  BMW.color = 'black';
-  BMW.name = '2023';
-  BMW.number = 543;
+  Human yoyo = Human();
+  yoyo.name = 'yuosif raed';
+  yoyo.height = 173;
+  yoyo.haierColor = 'brown';
 
-  print(BMW.name! + ' ' + BMW.number.toString() + ' ' + BMW.color!);
-
-  car sonata = car();
-  sonata.color = 'black';
-  sonata.name = '2022';
-  sonata.number = 543;
-  print(sonata.name! + ' ' + sonata.number.toString() + ' ' + sonata.color!);
-
-  Human ahmed = Human();
-  ahmed.haierColor = 'black';
-  ahmed.height = 32;
-  ahmed.myCar = car();
-  ahmed.myCar?.name = 'saibaa';
+  bedaa.PrintInformationHuman();
+  yoyo.PrintInformationHuman();
 }
 
 // objeect oriented programing
-//class
-class car {
-  String? name;
-  String? color;
-  num? number;
-}
 
 class Human {
   String? name;
   int? height;
   String? haierColor;
-  car? myCar;
+
+  void PrintInformationHuman() {
+    print(
+        'my name is : ${name} my hight is : ${height} my heair Color is : ${haierColor}');
+  }
 }
