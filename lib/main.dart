c0045f0c86533991bba9728a50b182266b23fbf7
Yuosif <yuosifraed@gmail.com>
2024-05-15 21:58:03 +0300

    complete examble

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,42 @@
-import 'Human.dart';
-
 void main() {
-  Human bedaa = Human('bedaa ali', 150, 'black');
-  bedaa.numberOfArams = 1;
+  //object
+  cat lolo = cat();
+  lolo.SkinColor = 'black';
 
-  print(bedaa.numberOfArams);
-  Human yoyo = Human('yuosif raed', 173, 'brown');
-  print(yoyo.numberOfArams);
+  lolo.meo();
+  lolo.CanWalk();
+  //object
+  dog jac = dog();
+  jac.SkinColor = 'black';
+
+  jac.barking();
+  jac.CanWalk();
+}
+
+// inhertance
+class cat extends Animals {
+  void meo() {
+    print('meo');
+  }
+}
+
+class lion extends Animals {
+  void roar() {
+    print('roar');
+  }
+}
+
+class dog extends Animals {
+  void barking() {
+    print('barking');
+  }
+}
+
+class Animals {
+  String? SkinColor;
+  double? Airms;
+
+  void CanWalk() {
+    print('WaLKING');
+  }
 }
